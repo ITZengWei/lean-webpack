753ac1945b7eccd91b1ab736d84f4b1ce97d5c2e
zengwei <2314978313@qq.com>
2021-11-05 09:47:02 +0800

    basic init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { sum } from './js/math'
+const { priceForm } = require('./js/format')
+
+console.log(sum(1, 2))
+console.log(priceForm(1, 2))
---
+++ src/js/format.js
@@ -0,0 +1,7 @@
+const priceForm = function () {
+  return '¥99.88'
+}
+
+module.exports = {
+  priceForm
+}
